94764e63882c04771c6fadc083a78c87586bbdf1
Khalid702 <69210413+Khalid702@users.noreply.github.com>
2021-07-19 16:41:28 +0300

    Add files via upload

---
+++ light-dark-mode/script.js
@@ -0,0 +1,59 @@
+const toggleSwitch = document.querySelector('input[type="checkbox"]');
+const nav = document.getElementById('nav');
+const toggleIcon = document.getElementById('toggle-icon');
+const image1 = document.getElementById('image1');
+const image2 = document.getElementById('image2');
+const image3 = document.getElementById('image3');
+const textBox = document.getElementById('text-box');
+
+function modes(navBg,boxBg,mode,star,color){
+	nav.style.backgroundColor = navBg;
+	textBox.style.backgroundColor = boxBg;
+    toggleIcon.children[0].textContent = `${mode} Mode`
+	toggleIcon.children[1].className = `fas fa-${star}`;
+	image1.src = `img/undraw_proud_coder_${color}.svg`;
+	image2.src = `img/undraw_feeling_proud_${color}.svg`;
+	image3.src = `img/undraw_conceptual_idea_${color}.svg`;	
+}
+
+
+
+//dark mode
+function darkMode(){
+ modes('rgb(0 0 0 / 50%)','rgb(255 255 255 / 50%)', 'Dark', 
+  'moon', 'dark');
+
+}
+//light mode
+function lightMode(){
+   modes('rgb(255 255 255 / 50%)','rgb(0 0 0 / 50%)','Light', 
+  'sun', 'light');	
+}
+
+
+//Switch Them Dynamically
+function switchTheme(event){
+if(event.target.checked){
+  document.documentElement.setAttribute('data-theme','dark');
+  localStorage.setItem('theme','dark');
+  darkMode();
+  } else {
+   document.documentElement.setAttribute('data-theme','light');
+   localStorage.setItem('theme','light');
+   lightMode();
+  }
+}
+
+//Event Listener
+toggleSwitch.addEventListener('change', switchTheme);
+
+//Check local storage for theme
+const currentTheme = localStorage.getItem('theme');
+if(currentTheme){
+document.documentElement.setAttribute('data-theme',currentTheme);
+ 
+  if(currentTheme === 'dark'){
+  	toggleSwitch.checked = true;
+ 	darkMode();
+    }
+  }
